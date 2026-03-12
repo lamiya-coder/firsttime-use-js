712f0a4e4cc27b4f272a38c9aeb259b07f06c49b
lammim200007-coder <lammim200007@gmail.com>
2026-03-12 10:14:03 +0600

    second commit

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const title = document.getElementById("title");
 const desc = document.getElementById("desc");
 const changeBtn = document.getElementById("change-button");
 const changeTheme = document.getElementById("theam-button");
-
 changeBtn.addEventListener("click", function () {
   title.textContent = " Ami parsi ";
   desc.textContent = " i'm doing great";
+});
+changeTheme.addEventListener("click", function () {
+  document.body.classList.toggle("black");
 });
